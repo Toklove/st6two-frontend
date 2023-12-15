b27dde6b9c84750b2bf43f5eb177d1cde45ea5ae
toklove <superz2586@gmail.com>
2023-12-15 17:59:46 +0800

    保存代码版本

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,38 +1,39 @@
 <template>
-    <div class="wrap" :class="`${className} ${isDark ? 'dark' : ''}`">
+    <div :class="`${className} ${isDark ? 'dark' : ''}`" class='wrap'>
         <!-- 导航条:开始 -->
         <TnNavbar
-            v-if="layoutData.showTopBar"
+            v-if='layoutData.showTopBar'
             :back-icon="layoutData.showTopBarBackBtn ? 'left' : ''"
+            :fixed='true'
             :home-icon="layoutData.showTopBarBackBtn ? 'home-capsule-fill' : ''"
-            :safe-area-inset-right="safeAreaInsetRight"
-            :fixed="true" frosted index-url="/pages/index"
+            :safe-area-inset-right='safeAreaInsetRight' frosted index-url='/pages/index'
         >
-            <text font-600 text="32px #333">{{ layoutData.topBarTitle }}</text>
+            <text font-600 text='32px #333'>{{ layoutData.topBarTitle }}</text>
         </TnNavbar>
         <!-- 导航条:结束 -->
         <!-- 不显示导航条时占位:开始 -->
-        <div v-else-if="layoutData.showTopBarPlaceholder" class="top-bar-placeholder" :style="`height:${navBarInfo}px`" />
+        <div v-else-if='layoutData.showTopBarPlaceholder' :style='`height:${navBarInfo}px`'
+             class='top-bar-placeholder' />
         <!-- 不显示导航条时占位:结束 -->
         <!-- 空白页面之上的插槽:开始 -->
-        <div class="header-slot flex-none">
-            <slot name="header-slot" />
+        <div class='header-slot flex-none'>
+            <slot name='header-slot' />
         </div>
         <!-- 空白页面之上的插槽:结束 -->
         <!-- 空白页面:开始 -->
-        <is-empty v-if="layoutData.showEmptySlot">
-            <slot name="empty-slot" />
+        <is-empty v-if='layoutData.showEmptySlot'>
+            <slot name='empty-slot' />
         </is-empty>
         <!-- 空白页面:结束 -->
         <!-- 页面主体:开始 -->
-        <div v-else class="layout-body flex-auto p-24px">
+        <div v-else class='layout-body flex-auto py-24px'>
             <slot />
         </div>
         <!-- 页面主体:结束 -->
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang='ts' setup>
 import type { LayoutDataType } from '~/types'
 
 defineOptions({
---
+++ src/pages/quotes.vue
@@ -0,0 +1,28 @@
+<template>
+    <page-meta :page-style='pageStyle' />
+    <div class='init-top' />
+    <layout class-name='IndexRouter'>
+        <view class='px-[34px]'>
+            <view class='flex items-center bg-[#f5f7f9] py-[31px] px-[30px] rounded-[50px]'>
+                <image class='w-[35px] h-[38px] mr-[21px]' src='/static/images/icon-search.png'></image>
+                <text class='text-[#525252] text-[28px]'>Enter the content here</text>
+            </view>
+            <view class='mt-[59px] relative'>
+                <view class='flex items-center justify-between'>
+                    <text class='text-[45px]'>Market</text>
+                    <text class='text-[20px]'>
+                        See more
+                    </text>
+                </view>
+            </view>
+        </view>
+    </layout>
+</template>
+
+<script lang='ts' setup>
+</script>
+
+<route lang='yaml'>
+style:
+navigationStyle: custom
+</route>
